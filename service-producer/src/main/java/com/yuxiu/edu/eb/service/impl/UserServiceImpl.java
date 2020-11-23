5df349e964e9b9e256cca85bf317505016e46ba1
chwy007 <394467690@qq.com>
2020-11-23 20:23:49 +0800

    Initial commit

---
+++ service-producer/src/main/java/com/yuxiu/edu/eb/service/impl/UserServiceImpl.java
@@ -0,0 +1,32 @@
+package com.yuxiu.edu.eb.service.impl;
+
+import com.yuxiu.edu.eb.service.IUserService;
+import com.yuxiu.edu.mapper.UserMapper;
+import com.yuxiu.edu.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+
+@Transactional
+public class UserServiceImpl implements IUserService{
+
+
+    @Autowired
+    private UserMapper userMapper;
+
+
+    @Override
+    public User findById(Integer id) {
+        return userMapper.findById(id);
+    }
+
+    @Override
+    public void register(String username, String password) {
+
+    }
+
+    public void setUserMapper(UserMapper userMapper) {
+        this.userMapper = userMapper;
+    }
+}
